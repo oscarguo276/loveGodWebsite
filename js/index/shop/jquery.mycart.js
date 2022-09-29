83e77c7bcdf98fa4c7f18cf0167cfea6d7db0b03
spadesAA <108362294+spadesAA@users.noreply.github.com>
2022-09-29 10:33:25 +0800

    商城
    更新可以關閉購物車和按鈕的顏色

--- js/index/shop/jquery.mycart.js
+++ js/index/shop/jquery.mycart.js
@@ -223,14 +223,14 @@
         '<div class="modal-dialog" role="document">' +
         '<div class="modal-content">' +
         '<div class="modal-header">' +
-        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+        '<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
         '<h4 class="modal-title" id="myModalLabel"><span class="glyphicon glyphicon-shopping-cart"></span> </h4>' +
         '</div>' +
         '<div class="modal-body">' +
         '<table class="table table-hover table-responsive" id="' + idCartTable + '"></table>' +
         '</div>' +
         '<div class="modal-footer">' +
-        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+        '<button type="button" class="btn btn-default" data-bs-dismiss="modal">Close</button>' +
         '<button type="button" class="btn btn-primary ' + classCheckoutCart + '">Checkout</button>' +
         '</div>' +
         '</div>' +
