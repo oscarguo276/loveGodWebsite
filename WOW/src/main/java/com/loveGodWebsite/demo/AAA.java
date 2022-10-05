8b7f52330210b69bf914d200fbbbdb27ee5f4291
oo999520 <108427623+oo999520@users.noreply.github.com>
2022-10-05 20:09:27 +0800

    測試用

---
+++ WOW/src/main/java/com/loveGodWebsite/demo/AAA.java
@@ -0,0 +1,10 @@
+package com.loveGodWebsite.demo;
+
+public class AAA {
+
+	public AAA() {
+		// TODO Auto-generated constructor stub
+		// 123132123
+	}
+
+}
---
+++ WOW/src/test/java/com/loveGodWebsite/demo/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveGodWebsite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
