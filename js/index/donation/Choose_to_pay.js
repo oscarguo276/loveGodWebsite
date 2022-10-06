bd4f8cb817e49dc4ce442ba07e992aa704d4dcf2
spadesAA <108362294+spadesAA@users.noreply.github.com>
2022-10-06 16:22:40 +0800

    香油錢
    教室傳送回家

--- js/index/donation/Choose_to_pay.js
+++ js/index/donation/Choose_to_pay.js
@@ -1,7 +1,7 @@
 var all_border = document.querySelector("#all_border");
 var signInButton = document.querySelector("#signIn");
 var signUpButton = document.querySelector("#signUp");
-var ATMtransfer = document.querySelector("#Payment2");
+var ATMtransfer = document.querySelector("#Payment1");
 
 
 ATMtransfer.addEventListener('click',function(){
@@ -9,12 +9,14 @@ ATMtransfer.addEventListener('click',function(){
     console.log("yes");
 })
 
-signInButton.addEventListener('click',function(){
-    all_border.classList.remove('right-panel-active')
-    console.log("yes");
-})
-
+//右邊開關
 signUpButton.addEventListener('click',function(){
     all_border.classList.add('right-panel-active')
     console.log("no");
+})
+
+//左邊開關
+signInButton.addEventListener('click',function(){
+    all_border.classList.remove('right-panel-active')
+    console.log("yes");
 })
